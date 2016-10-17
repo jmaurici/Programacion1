1ce94fad01f341d1a5218d2cf5e3c6585a899e93
mauricio <mauricio@DESKTOP-1PIPS59>
2016-10-17 09:14:29 +0100

    Se añade propiedad fechaIngreso en Estudiante

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -4,10 +4,20 @@ import java.util.Date;
 
 public class Estudiante extends Persona {
 	private int grupo;
+	String fechaIngreso;
 
-	public Estudiante(String nif, String nombre, char sexo, Date fecha, int grupo) {
+	public Estudiante(String nif, String nombre, char sexo, Date fecha, int grupo, String fechaIngreso) {
 		super(nif, nombre, sexo, fecha);
 		this.grupo = grupo;
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
 	}
 
 	public int getGrupo() {
