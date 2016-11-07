0ef3dc1a987ec19f9a3414c8bb1d15be2f18ea3f
arodi <arodi@DESKTOP-1PIPS59>
2016-11-07 10:03:48 +0000

    Muestra los valores introducidos en los textField en el TextArea

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -1,12 +1,23 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 
 public class Controlador {
 
+	@FXML
+	TextField min;
+	@FXML
+	TextField max;
+	@FXML
+	TextArea mensajes;
+	
 	public void manejaBoton (ActionEvent evt){
 		
-		System.out.println("Pulsado");
+	mensajes.setText(min.getText() + "," + max.getText());;
 		
 	}
 	
