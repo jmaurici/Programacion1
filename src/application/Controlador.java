b459f2c51427f19ccadfbef7d6081de310393912
arodi <arodi@DESKTOP-1PIPS59>
2016-11-07 10:05:46 +0000

    Revert "Muestra los valores introducidos en los textField en el TextArea"
    This reverts commit 0ef3dc1a987ec19f9a3414c8bb1d15be2f18ea3f.

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -1,23 +1,12 @@
 package application;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
 
 public class Controlador {
 
-	@FXML
-	TextField min;
-	@FXML
-	TextField max;
-	@FXML
-	TextArea mensajes;
-	
 	public void manejaBoton (ActionEvent evt){
 		
-	mensajes.setText(min.getText() + "," + max.getText());;
+		System.out.println("Pulsado");
 		
 	}
 	
--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,600,600);
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
